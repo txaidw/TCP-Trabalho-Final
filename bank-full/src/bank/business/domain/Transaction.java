1b91e0c733990d383d6088586ddc31aaedc982a3
Albert Kolberg <albert@kolberg.com.br>
2014-09-22 11:45:22 -0300

    first commit

---
+++ bank-full/src/bank/business/domain/Transaction.java
@@ -0,0 +1,76 @@
+package bank.business.domain;
+
+import java.util.Date;
+
+/**
+ * @author Ingrid Nunes
+ * 
+ */
+public abstract class Transaction {
+	
+	public enum State {
+		FINALIZED, PENDING;
+	}
+
+	private CurrentAccount account;
+	private double amount;
+	private Date date;
+	private OperationLocation location;
+	private State state;
+
+	protected Transaction(OperationLocation location, CurrentAccount account,
+			double amount) {
+		this.location = location;
+		this.date = new Date(System.currentTimeMillis());
+		this.account = account;
+		this.amount = amount;
+		this.state = State.PENDING;
+	}
+
+	/**
+	 * @return the account
+	 */
+	public CurrentAccount getAccount() {
+		return account;
+	}
+
+	/**
+	 * @return the amount
+	 */
+	public double getAmount() {
+		return amount;
+	}
+
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @return the location
+	 */
+	public OperationLocation getLocation() {
+		return location;
+	}
+
+	/**
+	 * This method is here for initializing the database.
+	 * 
+	 * @param date
+	 *            the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+}
